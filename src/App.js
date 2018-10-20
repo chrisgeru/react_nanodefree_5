553a17fe3ed353748f4bcf1876a8a04e49e048f8
Chris Maillefaud <chrismaillefaud@gmail.com>
2018-10-20 12:51:49 -0300

    Create components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Header } from "./Components/Header";
 
 /*
 This exercise will help you put together and practice all of the concepts you've
@@ -10,14 +10,136 @@ React and prepare you for your first project.
 The instructions for this project are located in the `instructions.md` file.
 */
 
+/*
+<PlayerGames>
+    <PlayerGamesMenu>
+        <AddNewPlayer>
+            <AddUsername>
+            <AddFirstName>
+            <AddSurname>
+        <OtherOptions>
+    <ListPlayers>
+        <SummaryList>
+        <PlayerList>
+            <Player>
+ */
+
+class ListPlayers extends Component {
+  render() {
+    return (
+      <div className="list-player-box">
+        <SummaryList />
+        <PlayerList />
+      </div>
+    );
+  }
+}
+
+class SummaryList extends Component {
+  render() {
+    return <h1>No players registered.</h1>;
+  }
+}
+
+class PlayerList extends Component {
+  render() {
+    return <Player />;
+  }
+}
+
+class Player extends Component {
+  render() {
+    return <p>Username played 0 games.</p>;
+  }
+}
+
+class PlayerGamesOptions extends Component {
+  render() {
+    return (
+      <div>
+        <AddNewPlayer />
+        <OtherOptions />
+      </div>
+    );
+  }
+}
+
+class OtherOptions extends Component {
+  render() {
+    return (
+        <div className="option-box">
+            <h1>Other Options</h1>
+            <button>Hide Games</button>
+        </div>
+
+    );
+  }
+}
+
+class AddNewPlayer extends Component {
+  render() {
+    return (
+      <div class="option-box">
+        <h1>Add New Player</h1>
+        <AddUserName />
+        <AddFirstName />
+        <AddSurName />
+        <button>Add Player</button>
+      </div>
+    );
+  }
+}
+
+class AddUserName extends Component {
+  render() {
+    return (
+      <div>
+        <div>Username:</div>
+        <input placeholder="Enter the username here" />
+      </div>
+    );
+  }
+}
+
+class AddFirstName extends Component {
+  render() {
+    return (
+      <div>
+        <div>First Name:</div>
+        <input placeholder="Enter the first name here" />
+      </div>
+    );
+  }
+}
+
+class AddSurName extends Component {
+  render() {
+    return (
+      <div>
+        <div>Surname:</div>
+        <input placeholder="Enter the surname here" />
+      </div>
+    );
+  }
+}
+
+class PlayerGames extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <PlayerGamesOptions />
+        <ListPlayers />
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-        </header>
+      <div>
+        <Header />
+        <PlayerGames />
       </div>
     );
   }
