917b23dd977bf6cc871255a6fad75a2cf16dbbc9
Chris Maillefaud <chrismaillefaud@gmail.com>
2018-10-30 21:14:29 -0300

    Create first states [wip]

--- src/App.js
+++ src/App.js
@@ -25,11 +25,17 @@ The instructions for this project are located in the `instructions.md` file.
  */
 
 class ListPlayers extends Component {
+  state = {
+    playerList: [
+      { userName: "chrismaille", firstName: "Chris", surName: "Maillefaud" }
+    ]
+  };
+
   render() {
     return (
       <div className="list-player-box">
-        <SummaryList />
-        <PlayerList />
+        <SummaryList playerCount={this.state.playerList.length} />
+        <PlayerList playerList={this.state.playerList} />
       </div>
     );
   }
@@ -37,19 +43,27 @@ class ListPlayers extends Component {
 
 class SummaryList extends Component {
   render() {
-    return <h1>No players registered.</h1>;
+    return this.props.playerCount === 0 ? (
+      <h1>No players registered.</h1>
+    ) : (
+      <h1>
+        {this.props.playerCount} player(s) saved.
+      </h1>
+    );
   }
 }
 
 class PlayerList extends Component {
   render() {
-    return <Player />;
+    return this.props.playerList.map(playerInfo => (
+      <Player info={playerInfo} />
+    ));
   }
 }
 
 class Player extends Component {
   render() {
-    return <p>Username played 0 games.</p>;
+    return <p>{this.props.info.userName} played 0 games.</p>;
   }
 }
 
@@ -67,57 +81,116 @@ class PlayerGamesOptions extends Component {
 class OtherOptions extends Component {
   render() {
     return (
-        <div className="option-box">
-            <h1>Other Options</h1>
-            <button>Hide Games</button>
-        </div>
-
+      <div className="option-box">
+        <h1>Other Options</h1>
+        <button>Hide Games</button>
+      </div>
     );
   }
 }
 
 class AddNewPlayer extends Component {
+  canAddPlayer = () => {
+    return !(
+      this.state.firstName !== "" &&
+      this.state.userName !== "" &&
+      this.state.surName !== ""
+    );
+  };
+
+  state = {
+    firstName: "",
+    surName: "",
+    userName: ""
+  };
+
+  updateForm = payload => {
+    this.setState(payload);
+  };
+
   render() {
     return (
       <div class="option-box">
         <h1>Add New Player</h1>
-        <AddUserName />
-        <AddFirstName />
-        <AddSurName />
-        <button>Add Player</button>
+        <AddUserName onChangeHandle={this.updateForm} />
+        <AddFirstName onChangeHandle={this.updateForm} />
+        <AddSurName onChangeHandle={this.updateForm} />
+        <button disabled={this.canAddPlayer()}>Add Player</button>
       </div>
     );
   }
 }
 
 class AddUserName extends Component {
+  state = {
+    userName: ""
+  };
+
+  handleChange = event => {
+    this.setState({ userName: event.target.value }, () =>
+      this.props.onChangeHandle(this.state)
+    );
+  };
+
   render() {
     return (
       <div>
         <div>Username:</div>
-        <input placeholder="Enter the username here" />
+        <input
+          placeholder="Enter the username"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
 }
 
 class AddFirstName extends Component {
+  state = {
+    firstName: ""
+  };
+
+  handleChange = event => {
+    this.setState({ firstName: event.target.value }, () =>
+      this.props.onChangeHandle(this.state)
+    );
+  };
+
   render() {
     return (
       <div>
         <div>First Name:</div>
-        <input placeholder="Enter the first name here" />
+        <input
+          placeholder="Enter the first name"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
 }
 
 class AddSurName extends Component {
+  state = {
+    surName: ""
+  };
+
+  handleChange = event => {
+    this.setState({ surName: event.target.value }, () =>
+      this.props.onChangeHandle(this.state)
+    );
+  };
+
   render() {
     return (
       <div>
         <div>Surname:</div>
-        <input placeholder="Enter the surname here" />
+        <input
+          placeholder="Enter the surname"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
